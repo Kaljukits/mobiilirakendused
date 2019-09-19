caf559ec0a76b4552aaad43b63c102e51e5cee30
Kaljukits <minnamiinu@outlook.com>
2019-09-19 09:49:58 +0300

    19.09.2019

---
+++ XamarinEE/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Xamarin.Essentials;
+
+namespace XamarinEE
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView _batteryLevelView;
+        TextView _state;
+        TextView _charging;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            _batteryLevelView = FindViewById<TextView>(Resource.Id.textView2);
+            _state = FindViewById<TextView>(Resource.Id.textView4);
+            _charging = FindViewById<TextView>(Resource.Id.textView5);
+            var batterylevel = Battery.ChargeLevel;
+            _batteryLevelView.Text = batterylevel.ToString();
+
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+        }
+
+        private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            _batteryLevelView.Text = Battery.ChargeLevel.ToString();
+            _state.Text = Battery.State.ToString();
+            //_charging = Battery.PowerSource
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
