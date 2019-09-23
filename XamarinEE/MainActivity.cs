b9b28c004692b8b6e05135167a219f4137127598
Kaljukits <minnamiinu@outlook.com>
2019-09-23 19:14:49 +0300

    Kodutöö 24.09.19

--- XamarinEE/MainActivity.cs
+++ XamarinEE/MainActivity.cs
@@ -12,7 +12,7 @@ namespace XamarinEE
     {
         TextView _batteryLevelView;
         TextView _state;
-        TextView _charging;
+        TextView _source;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,18 +23,101 @@ namespace XamarinEE
 
             _batteryLevelView = FindViewById<TextView>(Resource.Id.textView2);
             _state = FindViewById<TextView>(Resource.Id.textView4);
-            _charging = FindViewById<TextView>(Resource.Id.textView5);
+            _source = FindViewById<TextView>(Resource.Id.textView5);
             var batterylevel = Battery.ChargeLevel;
-            _batteryLevelView.Text = batterylevel.ToString();
+            float BLF = float.Parse(batterylevel.ToString()) * 100;
+            _batteryLevelView.Text = BLF.ToString() + "%";
+
+            var state = Battery.State;
+            switch (state)
+            {
+                case BatteryState.Charging:
+                    _state.Text = "Currently charging";
+                    break;
+                case BatteryState.Full:
+                    _state.Text = "Battery is full";
+                    break;
+                case BatteryState.Discharging:
+                case BatteryState.NotCharging:
+                    _state.Text = "Currently discharging battery or not being charged";
+                    break;
+                case BatteryState.NotPresent:
+                    _state.Text = "Battery doesn't exist in device (desktop computer)";
+                    break;
+                case BatteryState.Unknown:
+                    _state.Text = "Unable to detect battery state";
+                    break;
+            }
+
+            var source = Battery.PowerSource;
+            switch (source)
+            {
+                case BatteryPowerSource.Battery:
+                    _source.Text = "Being powered by the battery";
+                    break;
+                case BatteryPowerSource.AC:
+                    _source.Text = "Being powered by A/C unit";
+                    break;
+                case BatteryPowerSource.Usb:
+                    _source.Text = "Being powered by USB cable";
+                    break;
+                case BatteryPowerSource.Wireless:
+                    _source.Text = "Powered via wireless charging";
+                    break;
+                case BatteryPowerSource.Unknown:
+                    _source.Text = "Unable to detect power source";
+                    break;
+            }
 
             Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
         }
 
         private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
         {
-            _batteryLevelView.Text = Battery.ChargeLevel.ToString();
-            _state.Text = Battery.State.ToString();
-            //_charging = Battery.PowerSource
+            float BLF = float.Parse(Battery.ChargeLevel.ToString()) * 100;
+            _batteryLevelView.Text = BLF.ToString() + "%";
+
+            var state = Battery.State;
+            switch (state)
+            {
+                case BatteryState.Charging:
+                    _state.Text = "Currently charging";
+                    break;
+                case BatteryState.Full:
+                    _state.Text = "Battery is full";
+                    break;
+                case BatteryState.Discharging:
+                case BatteryState.NotCharging:
+                    _state.Text = "Currently discharging battery or not being charged";
+                    break;
+                case BatteryState.NotPresent:
+                    _state.Text = "Battery doesn't exist in device (desktop computer)";
+                    break;
+                case BatteryState.Unknown:
+                    _state.Text = "Unable to detect battery state";
+                    break;
+            }
+
+            var source = Battery.PowerSource;
+            switch (source)
+            {
+                case BatteryPowerSource.Battery:
+                    _source.Text = "Being powered by the battery";
+                    break;
+                case BatteryPowerSource.AC:
+                    _source.Text = "Being powered by A/C unit";
+                    break;
+                case BatteryPowerSource.Usb:
+                    _source.Text = "Being powered by USB cable";
+                    break;
+                case BatteryPowerSource.Wireless:
+                    _source.Text = "Powered via wireless charging";
+                    break;
+                case BatteryPowerSource.Unknown:
+                    _source.Text = "Unable to detect power source";
+                    break;
+            }
+
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
