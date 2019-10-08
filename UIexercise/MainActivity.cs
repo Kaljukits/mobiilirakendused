36cfad2b117f1292af8a8e1354bdd561724ecc4c
Kaljukits <minnamiinu@outlook.com>
2019-10-08 09:18:41 +0300

    Add 08.10.2019 work

--- UIexercise/MainActivity.cs
+++ UIexercise/MainActivity.cs
@@ -3,12 +3,15 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Webkit;
+using Android.Views;
 
 namespace UIexercise
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        WebView _webView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -19,6 +22,20 @@ namespace UIexercise
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetActionBar(toolbar);
             ActionBar.Title = "My Toolbar";
+
+            _webView = FindViewById<WebView>(Resource.Id.webview);
+            _webView.Settings.JavaScriptEnabled = true;
+            _webView.SetWebViewClient(new HelloWebViewClient());
+            _webView.LoadUrl("https://www.neti.ee/");
+        }
+        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && _webView.CanGoBack())
+            {
+                _webView.GoBack();
+                return true;
+            }
+                return base.OnKeyDown(keyCode, e);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -26,5 +43,6 @@ namespace UIexercise
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
     }
 }
